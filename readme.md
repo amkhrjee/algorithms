73550c2f2cd7fd09df4c071609607a2229199e36
amkhrjee <aniruddha.mukherji@outlook.com>
2023-03-18 20:49:12 +0530

    initial

---
+++ readme.md
@@ -0,0 +1,5 @@
+# algorithms
+
+This repo contains a collection of different algorithms, majorly from the book "Introduction to Algorithms" by CLRS. It also contains solutions to intersting algorithm problems that I have encountered, either inside or outside university workload.
+
+I will be glad if you find this of any help.
---
+++ special/closestpair.cpp
@@ -0,0 +1,230 @@
+#include <iostream>
+#include <cmath>
+#define num_of_comparisons 7
+using namespace std;
+
+struct point
+{
+    double x, y;
+    point(double xval = 0, double yval = 0) : x(xval), y(yval) {}
+};
+
+struct closest_pair
+{
+    point p_1, p_2;
+    double distance;
+    closest_pair(point first, point second, double dist) : distance(dist)
+    {
+        if (first.x > second.x || (first.x == second.x && first.y > second.y))
+        {
+            p_1 = first;
+            p_2 = second;
+        }
+        else
+        {
+            p_1 = second;
+            p_2 = first;
+        }
+    }
+};
+
+double get_dist(point p_1, point p_2)
+{
+    return sqrt(pow((p_1.x - p_2.x), 2) + pow((p_1.y - p_2.y), 2));
+}
+
+void merge(point *point_list, int left, int mid, int right, bool wrt_x = false)
+{
+    int i = left;
+    int j = mid + 1;
+    int index = 0;
+    point tempArr[right - left + 1];
+    while (i <= mid && j <= right)
+    {
+        if (wrt_x == true)
+        {
+            if (point_list[i].x <= point_list[j].x)
+            {
+                tempArr[index++] = point_list[i++];
+            }
+            else if (point_list[i].x >= point_list[j].x)
+            {
+                tempArr[index++] = point_list[j++];
+            }
+        }
+        // wrt y case
+        else
+        {
+            if (point_list[i].y <= point_list[j].y)
+            {
+                tempArr[index++] = point_list[i++];
+            }
+            else if (point_list[i].y >= point_list[j].y)
+            {
+                tempArr[index++] = point_list[j++];
+            }
+        }
+    }
+
+    while (i <= mid)
+    {
+        tempArr[index++] = point_list[i++];
+    }
+    while (j <= right)
+    {
+        tempArr[index++] = point_list[j++];
+    }
+
+    for (int i = 0; i < index; i++)
+    {
+        point_list[left + i] = tempArr[i];
+    }
+    // delete[] tempArr;
+}
+
+void sort_points(point *point_list, int left, int right, bool wrt_x = false, bool wrt_y = false)
+{
+    // sort either wrt_x or wrt_y
+    if (wrt_x == true && wrt_y == true)
+    {
+        cerr << "Sort error: cannot sort both wrt x and y" << endl;
+        exit(1);
+    }
+    else if (wrt_x == false && wrt_y == false)
+    {
+        cerr << "Sort error: one of x and y must be selected" << endl;
+        exit(1);
+    }
+    else if (wrt_x == true)
+    {
+        if (left < right)
+        {
+            // sort wrt x
+            int mid = (left + right) / 2;
+            sort_points(point_list, left, mid, true, false);
+            sort_points(point_list, mid + 1, right, true, false);
+            merge(point_list, left, mid, right, true);
+        }
+    }
+    else if (wrt_y == true)
+    {
+        if (left < right)
+        {
+            // sort wrt x
+            int mid = (left + right) / 2;
+            sort_points(point_list, left, mid, false, true);
+            sort_points(point_list, mid + 1, right, false, true);
+            merge(point_list, left, mid, right, false);
+        }
+    }
+}
+
+double get_min_dist(double v_1, double v_2)
+{
+    if (v_1 <= v_2)
+        return v_1;
+    else
+        return v_2;
+}
+
+closest_pair get_closest_pair(point *x_sorted_list, point *y_sorted_list, int len)
+{
+    if (len == 1)
+    {
+        cerr << "Input Error: Only one point given" << endl;
+        exit(1);
+    }
+    else if (len == 2)
+    {
+        // cout << "hello" << endl;
+        return closest_pair(x_sorted_list[0],
+                            x_sorted_list[1],
+                            get_dist(x_sorted_list[0], x_sorted_list[1]));
+    }
+    else if (len == 3)
+    {
+        // return minimum of the three possible distances
+        double dist_1 = get_dist(x_sorted_list[0], x_sorted_list[1]);
+        double dist_2 = get_dist(x_sorted_list[0], x_sorted_list[2]);
+        double dist_3 = get_dist(x_sorted_list[1], x_sorted_list[1]);
+
+        double temp_min_dist = get_min_dist(dist_1, dist_2);
+        temp_min_dist = get_min_dist(temp_min_dist, dist_3);
+
+        if (dist_1 == temp_min_dist)
+        {
+            return closest_pair(x_sorted_list[0], x_sorted_list[1], temp_min_dist);
+        }
+        else if (dist_2 == temp_min_dist)
+        {
+            return closest_pair(x_sorted_list[0], x_sorted_list[2], temp_min_dist);
+        }
+        else
+        {
+            return closest_pair(x_sorted_list[1], x_sorted_list[2], temp_min_dist);
+        }
+    }
+    else
+    {
+
+        // cout << "hello from else" << endl;
+        double left_min_dist = get_closest_pair(x_sorted_list, y_sorted_list, len / 2).distance;
+        double right_min_dist = get_closest_pair(x_sorted_list + len / 2, y_sorted_list, len - len / 2).distance;
+        double min_dist = get_min_dist(left_min_dist, right_min_dist);
+
+        closest_pair final_pair = {0, 0, 0};
+        // perform comparisons in the strip
+        for (int i = 0; i < len; i++)
+        {
+            if (y_sorted_list[i].x <= x_sorted_list[len - 1].x && y_sorted_list[i].x >= x_sorted_list[0].x)
+            {
+                // compare with the next seven points
+                for (int j = 1; (j <= num_of_comparisons) && ((i + j) < len); j++)
+                {
+                    double dist_in_strip = get_dist(y_sorted_list[i], y_sorted_list[i + j]);
+                    if (dist_in_strip <= min_dist)
+                    {
+                        min_dist = dist_in_strip;
+                        final_pair = {y_sorted_list[i], y_sorted_list[i + j], dist_in_strip};
+                    }
+                }
+            }
+        }
+        return final_pair;
+    }
+}
+
+int main()
+{
+    // sample points
+    point point_list[] = {{1, 2},
+                          {30, 40},
+                          {-4, 5},
+                          {6, -7},
+                          {-8, -9},
+                          {-10, 11},
+                          {11, -12},
+                          {-13, -14},
+                          {-15, 16},
+                          {-17, 18}};
+
+    int len = sizeof(point_list) / sizeof(point);
+    point sorted_wrt_x[len];
+    for (int i = 0; i < len; i++)
+    {
+        sorted_wrt_x[i] = point_list[i];
+    }
+    point sorted_wrt_y[len];
+    for (int i = 0; i < len; i++)
+    {
+        sorted_wrt_y[i] = point_list[i];
+    }
+
+    // rationale behind separate flags for wrt_x and wrt_y: more clarity
+    sort_points(sorted_wrt_x, 0, len - 1, true, false);
+    sort_points(sorted_wrt_y, 0, len - 1, false, true);
+
+    closest_pair cp = get_closest_pair(sorted_wrt_x, sorted_wrt_y, len);
+
+    cout << "Closest pair: p_1(" << cp.p_1.x << ", " << cp.p_1.y << "), p_2(" << cp.p_2.x << ", " << cp.p_2.y << ") d = " << cp.distance << endl;
+}
